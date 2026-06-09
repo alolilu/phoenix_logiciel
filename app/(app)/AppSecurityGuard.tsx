cded0525fb6328ff762f4127db7da6427765dc4b
Alexis <phoenixnouvelleaquitaine@gmail.com>
2026-06-09 10:18:50 +0200

    Test

--- app/(app)/AppSecurityGuard.tsx
+++ app/(app)/AppSecurityGuard.tsx
@@ -1,27 +1,27 @@
-"use client";
+// "use client";
 
-import { useEffect } from "react";
-import { signOut } from "next-auth/react";
+// import { useEffect } from "react";
+// import { signOut } from "next-auth/react";
 
-export default function AppSecurityGuard() {
-  useEffect(() => {
-    const originalFetch = window.fetch;
+// export default function AppSecurityGuard() {
+//   useEffect(() => {
+//     const originalFetch = window.fetch;
 
-    window.fetch = async (...args) => {
-      const response = await originalFetch(...args);
+//     window.fetch = async (...args) => {
+//       const response = await originalFetch(...args);
 
-      if (response.status === 401 || response.status === 403) {
-        // évite boucle infinie si déjà en train de logout
-        await signOut({ callbackUrl: "/login" });
-      }
+//       if (response.status === 401 || response.status === 403) {
+//         // évite boucle infinie si déjà en train de logout
+//         await signOut({ callbackUrl: "/login" });
+//       }
 
-      return response;
-    };
+//       return response;
+//     };
 
-    return () => {
-      window.fetch = originalFetch;
-    };
-  }, []);
+//     return () => {
+//       window.fetch = originalFetch;
+//     };
+//   }, []);
 
-  return null;
-}
+//   return null;
+// }
